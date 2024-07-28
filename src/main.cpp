dd1cdc7b547ff3f45fca914c5f490ac11b881ced
hwthinker <hwthinker@gmail.com>
2024-07-28 20:51:45 +0700

    first commit

--- src/main.cpp
+++ src/main.cpp
@@ -4,6 +4,10 @@
 #define RLY2     32
 #define RLY3     33
 #define RLY4     25
+#define RLY5     27
+#define RLY6     12
+#define RLY7     14
+#define RLY8     35
 #define LED      26
 
 
@@ -12,14 +16,18 @@ void setup() {
   pinMode(RLY2, OUTPUT);  //relay2
   pinMode(RLY3, OUTPUT);  //relay3
   pinMode(RLY4, OUTPUT); //relay4
-  pinMode(LED, OUTPUT); //relay4
+  pinMode(RLY5, OUTPUT);  //relay1
+  pinMode(RLY6, OUTPUT);  //relay2
+  pinMode(RLY7, OUTPUT);  //relay3
+  pinMode(RLY8, OUTPUT); //relay4
+  pinMode(LED, OUTPUT); 
 }
 
 
 // the loop function runs over and over again forever
 void loop() {
   digitalWrite(RLY1, HIGH);  // turn the LED on (HIGH is the voltage level)
-  delay(1000);                      // wait for a second
+  delay(1000);               // wait for a second
   digitalWrite(RLY1, LOW);   // turn the LED off by making the voltage LOW
   delay(1000);    
 
@@ -37,6 +45,26 @@ void loop() {
   delay(1000);                      // wait for a second
   digitalWrite(RLY4, LOW);   // turn the LED off by making the voltage LOW
   delay(1000);   
+
+  digitalWrite(RLY5, HIGH);  // turn the LED on (HIGH is the voltage level)
+  delay(1000);                      // wait for a second
+  digitalWrite(RLY5, LOW);   // turn the LED off by making the voltage LOW
+  delay(1000);    
+
+  digitalWrite(RLY6, HIGH);  // turn the LED on (HIGH is the voltage level)
+  delay(1000);                      // wait for a second
+  digitalWrite(RLY6, LOW);   // turn the LED off by making the voltage LOW
+  delay(1000);   
+
+  digitalWrite(RLY7, HIGH);  // turn the LED on (HIGH is the voltage level)
+  delay(1000);                      // wait for a second
+  digitalWrite(RLY7, LOW);   // turn the LED off by making the voltage LOW
+  delay(1000);   
+
+  digitalWrite(RLY8, HIGH);  // turn the LED on (HIGH is the voltage level)
+  delay(1000);                      // wait for a second
+  digitalWrite(RLY8, LOW);   // turn the LED off by making the voltage LOW
+  delay(1000); 
   
   digitalWrite(LED, HIGH);  // turn the LED on (HIGH is the voltage level)
   delay(1000);                      // wait for a second
